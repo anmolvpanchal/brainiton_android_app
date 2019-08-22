88af555f4da182517ace997f13290b6b87f2ba66
dhruvam patel <dhruvampatel@dhruvams-MacBook-Air.local>
2019-08-22 11:18:12 +0530

    Full notification done

--- app/src/main/java/com/combrainiton/utils/FirebaseNotificationService.java
+++ app/src/main/java/com/combrainiton/utils/FirebaseNotificationService.java
@@ -14,6 +14,7 @@ import android.support.v4.app.NotificationCompat;
 import com.combrainiton.R;
 import com.combrainiton.main.ActivityNavExplore;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -27,10 +28,11 @@ public class FirebaseNotificationService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage message) {
 
+        super.onMessageReceived(message);
+
         Context context = this;
         NotificationManagerCompat notificationManagerCompat;
 
-        //super.onMessageReceived(message);
         Intent intent = new Intent(this, ActivityNavExplore.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
@@ -50,32 +52,11 @@ public class FirebaseNotificationService extends FirebaseMessagingService {
                 .build();
 
         notificationManagerCompat.notify(1,notification);
-
-        Log.i("Firebase","yessssss");
-
-
-
-        /*NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,"28+")
-                .setContentTitle(Objects.requireNonNull(message.getNotification()).getTitle() + "")
-                .setContentText(message.getNotification().getBody())
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setStyle(new NotificationCompat.BigTextStyle())
-                .setSmallIcon(R.drawable.ic_app_logo_notification)
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_app_logo_notification))
-                .setContentIntent(pendingIntent)
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setAutoCancel(true);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1, notificationBuilder.build());*/
-
-
     }
 
     @Override
     public void onNewToken(String s) {
-        Log.i("FirebaseToken", FirebaseInstanceId.getInstance().getToken());
+        Log.i("FirebaseToken", s);
     }
 }
 
