d58704cad26fec2617d04368cbfa15473687833c
developerbrainiton3 <3devbrainiton@gmail.com>
2019-08-23 11:03:55 +0530

    Notification with image

--- app/src/main/java/com/combrainiton/utils/FirebaseNotificationService.java
+++ app/src/main/java/com/combrainiton/utils/FirebaseNotificationService.java
@@ -28,18 +28,21 @@ public class FirebaseNotificationService extends FirebaseMessagingService {
 
     String title,text;
     Uri img_url;
+    Bitmap bitmap = null;
+    final String CHANNEL_ID = "28+";
 
     @Override
     public void onMessageReceived(RemoteMessage message) {
 
         super.onMessageReceived(message);
 
+        //Getting values from incoming notification
         title = message.getNotification().getTitle();
         text = message.getNotification().getBody();
         img_url = message.getNotification().getImageUrl();
 
         //Convert image uri to bitmap
-        Bitmap bitmap = null;
+
         try {
             bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), img_url);
         } catch (IOException e) {
@@ -50,8 +53,23 @@ public class FirebaseNotificationService extends FirebaseMessagingService {
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"28+")
+        //If android version is above Oreo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotification(pendingIntent,CHANNEL_ID);
+        } else { //If android version is below Oreo
+            createNotification(pendingIntent);
+        }
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        Log.i("FirebaseToken", s);
+    }
+
+    private void createNotification(PendingIntent pendingIntent,String channel_id){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,channel_id)
                 .setContentTitle(title)
+                .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setStyle(new NotificationCompat.BigPictureStyle()
                         .bigPicture(bitmap)
@@ -70,9 +88,26 @@ public class FirebaseNotificationService extends FirebaseMessagingService {
         manager.notify(0,builder.build());
     }
 
-    @Override
-    public void onNewToken(String s) {
-        Log.i("FirebaseToken", s);
+    private void createNotification(PendingIntent pendingIntent){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"28+")
+                .setContentTitle(title)
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(bitmap)
+                        .bigLargeIcon(null))
+                .setSmallIcon(R.drawable.ic_app_logo_notification) //meta-data has been made in manifest.xml for icon
+                .setContentIntent(pendingIntent)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setAutoCancel(true);
+
+        //if image is available convert to bitmap and set it else leave
+        if(bitmap != null){
+            builder.setLargeIcon(bitmap);
+        }
+
+        NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0,builder.build());
     }
 }
 
