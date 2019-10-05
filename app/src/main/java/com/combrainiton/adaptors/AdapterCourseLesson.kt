b5c53fbf84ea1bcd87f20bfdacd0abe35e175a59
developerbrainiton3 <3devbrainiton@gmail.com>
2019-10-05 16:20:34 +0530

    Api call for getting quiz id from my subscription

--- app/src/main/java/com/combrainiton/adaptors/AdapterCourseLesson.kt
+++ app/src/main/java/com/combrainiton/adaptors/AdapterCourseLesson.kt
@@ -8,19 +8,28 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Filter
-import android.widget.Filterable
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.combrainiton.R
-import com.combrainiton.models.GetAllQuizResponceModel
-import com.combrainiton.normalQuiz.ActivityNormalQuizDescription
+import com.combrainiton.normalQuiz.ActivityNormalQuizInstruction
 import com.combrainiton.subscription.LessonsDataList_API
+import com.combrainiton.subscription.ServiceGenerator
 import com.combrainiton.subscription.SubscriptionDataList_API
-import kotlinx.android.synthetic.main.card_view_report_item.view.*
+import com.combrainiton.subscription.SubscriptionInterface
+import com.combrainiton.utils.AppAlerts
+import com.combrainiton.utils.AppSharedPreference
 import kotlinx.android.synthetic.main.course_lessons_card_view_item.view.*
+import okhttp3.ResponseBody
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class AdapterCourseLesson (var mContext: Context, var mActivity: Activity, val lessonsDataList: ArrayList<LessonsDataList_API>, val subscriptionDataList: ArrayList<SubscriptionDataList_API>) : androidx.recyclerview.widget.RecyclerView.Adapter<AdapterCourseLesson.MyViewHolder>(){
+class AdapterCourseLesson(var mContext: Context, var mActivity: Activity, val lessonsDataList: ArrayList<LessonsDataList_API>, val subscriptionDataList: ArrayList<SubscriptionDataList_API>) : androidx.recyclerview.widget.RecyclerView.Adapter<AdapterCourseLesson.MyViewHolder>() {
+
+    var quiz_id : String = ""
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
@@ -43,6 +52,16 @@ class AdapterCourseLesson (var mContext: Context, var mActivity: Activity, val l
         //Setting lesson count
         val no = position + 1
         holder.lessonCount.text = no.toString()
+
+       holder.Card.setOnClickListener {
+
+           val lessonIDtoPass = lessonsDataList.get(position).lessonId
+           val quiz_name = lessonsDataList.get(position).lessonName.toString()
+           getLessonsFromApi(lessonIDtoPass.toString(),quiz_name)
+
+
+
+       }
 
         //Removing UpperLine and LowerLine from first and last card
         /*if(position == 0){ //First card
@@ -77,5 +96,69 @@ class AdapterCourseLesson (var mContext: Context, var mActivity: Activity, val l
         val lessonCount = mView.course_lessons_lessons_count
         val upperLine = mView.course_lessons_upper_line
         val lowerLine = mView.course_lessons_lower_line
+        val Card = mView.my_quizzes_list_item_main_container
     }
+
+    fun getLessonsFromApi(lessonID: String, quizName: String) {
+
+        //create api client first
+        val apiToken: String = AppSharedPreference(mContext).getString("apiToken")
+
+
+        val apiClient = ServiceGenerator.getClient(apiToken).create(SubscriptionInterface::class.java)
+        val call = apiClient.getQuizID(lessonID.toInt())
+
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                //mProgressDialog.dialog.dismiss()
+                AppAlerts().showAlertMessage(mContext, "Error", mActivity.resources.getString(R.string.error_server_problem))
+            }
+
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+
+                if (!response.isSuccessful()) {
+                    Toast.makeText(mContext, "Something went Wrong !!" + response.code(), Toast.LENGTH_SHORT).show();
+                    Log.e("!response.isSuccessful", "body \n"
+                            + response.errorBody().toString()
+                            + " code ${response.code()}")
+                    return;
+                }
+
+                try {
+
+                    val resp = response.body()?.string()
+                    val rootObj = JSONObject(resp)
+                    val play = rootObj.getString("play")
+
+                    if (play.equals("false")){
+                        Toast.makeText(mContext, "Quiz Will be available tomorrow " , Toast.LENGTH_LONG).show();
+                    }else{
+                         quiz_id = rootObj.getString("quiz_id")
+
+                        Log.e("AdapterResult","responce" + quiz_id)
+
+//                        mActivity.startActivity(Intent(mActivity, ActivityNormalQuizInstruction::class.java)
+//                                .putExtra("quizId", quiz_id) //pass quiz id
+//                                .putExtra("quizName", quizName)) //pass quiz name
+
+                    }
+
+
+                } catch (ex: Exception) {
+                    when (ex) {
+                        is IllegalAccessException, is IndexOutOfBoundsException -> {
+                            Log.e("catch block", "some known exception" + ex)
+                        }
+                        else -> Log.e("catch block", "other type of exception" + ex)
+
+                    }
+
+                }
+            }
+
+        })
+
+
+    }
+
 }
